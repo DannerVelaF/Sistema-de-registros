3c93f3c6d4bc54c59a36c4f75fad1c03a32bcb48
DannerVelaF <danner.alj96@hotmail.com>
2023-10-22 02:25:58 -0500

    cambios en scrpt.js

--- script/script.js
+++ script/script.js
@@ -1,38 +1,50 @@
+//Declaracion de los elementos a manipular
+const contenedor_principal = document.getElementById("principal");
+const inicio = document.getElementById("inicio");
+const cursos = document.getElementById("cursos");
+const estudiantes = document.getElementById("estudiantes");
+const inscripciones = document.getElementById("inscripciones");
+const perfil = document.getElementById("perfil");
+const cerrar_sesion = document.getElementById("cerrar_sesion");
+
+// Funcion que permite llamar las paginas correspondientes dentro del contenedor principal
 function cargarContenido(page) {
   fetch(page)
     .then((res) => res.text())
     .then((data) => {
-      document.getElementById("principal").innerHTML = data;
+      contenedor_principal.innerHTML = data;
     })
     .catch((err) => {
       console.err(err);
     });
 }
-document.getElementById("inicio").addEventListener("click", (evt) => {
+
+// Llamado de las paginas mediante un click en los botones del dashboard
+inicio.addEventListener("click", (evt) => {
   evt.preventDefault();
   location.reload();
 });
 
-document.getElementById("cursos").addEventListener("click", (evt) => {
+cursos.addEventListener("click", (evt) => {
   evt.preventDefault();
   cargarContenido("/pages/cursos.html");
 });
 
-document.getElementById("estudiantes").addEventListener("click", (evt) => {
+estudiantes.addEventListener("click", (evt) => {
   evt.preventDefault();
   cargarContenido("/pages/estudiantes.html");
 });
-document.getElementById("inscripciones").addEventListener("click", (evt) => {
+inscripciones.addEventListener("click", (evt) => {
   evt.preventDefault();
   cargarContenido("/pages/incripciones.html");
 });
 
-document.getElementById("perfil").addEventListener("click", (evt) => {
+perfil.addEventListener("click", (evt) => {
   evt.preventDefault();
   cargarContenido("/pages/perfil.html");
 });
 
-document.getElementById("cerrar_sesion").addEventListener("click", (evt) => {
+cerrar_sesion.addEventListener("click", (evt) => {
   evt.preventDefault();
   window.history.back();
 });
