db82ace84bf959a0be8323f2d200c30e04663a27
DannerVelaF <danner.alj96@hotmail.com>
2023-10-24 18:58:34 -0500

    Agregado en la seccion cursos

--- script/script.js
+++ script/script.js
@@ -6,6 +6,8 @@ const estudiantes = document.getElementById("estudiantes");
 const inscripciones = document.getElementById("inscripciones");
 const perfil = document.getElementById("perfil");
 const cerrar_sesion = document.getElementById("cerrar_sesion");
+const agregar_curso = document.getElementById("agregar-curso");
+const ver_curso = document.getElementById("ver-cursos");
 
 // Funcion que permite llamar las paginas correspondientes dentro del contenedor principal
 function cargarContenido(page) {
@@ -19,28 +21,49 @@ function cargarContenido(page) {
     });
 }
 
-// Llamado de las paginas mediante un click en los botones del dashboard
+function ocultarCursos() {
+  agregar_curso.style.display = "none";
+  ver_curso.style.display = "none";
+}
+
 inicio.addEventListener("click", (evt) => {
   evt.preventDefault();
   location.reload();
 });
 
+//Cursos
 cursos.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  agregar_curso.style.display = "flex";
+  ver_curso.style.display = "flex";
+  cargarContenido("/pages/cursos.html");
+});
+
+agregar_curso.addEventListener("click", (evt) => {
   evt.preventDefault();
   cargarContenido("/pages/cursos.html");
 });
 
+ver_curso.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  cargarContenido("/pages/ver_cursos.html");
+});
+//Cursos
+
 estudiantes.addEventListener("click", (evt) => {
   evt.preventDefault();
+  ocultarCursos();
   cargarContenido("/pages/estudiantes.html");
 });
 inscripciones.addEventListener("click", (evt) => {
   evt.preventDefault();
+  ocultarCursos();
   cargarContenido("/pages/incripciones.html");
 });
 
 perfil.addEventListener("click", (evt) => {
   evt.preventDefault();
+  ocultarCursos();
   cargarContenido("/pages/perfil.html");
 });
 
